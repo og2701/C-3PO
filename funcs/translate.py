2a0687d0e0e3ae3b6f03136d677917dc36758da9
og2701 <67282414+og2701@users.noreply.github.com>
2024-06-09 12:34:34 +0100

    Update translate.py

--- funcs/translate.py
+++ funcs/translate.py
@@ -15,7 +15,7 @@ async def translate(interaction: discord.Interaction, message: str):
         current_width = 0
         
         for word in words:
-            word_width, _ = font.getsize(word + ' ')
+            word_width, _ = font.getbbox(word + ' ')[2:]
             if current_width + word_width <= max_width:
                 current_line.append(word)
                 current_width += word_width
@@ -31,14 +31,14 @@ async def translate(interaction: discord.Interaction, message: str):
     translated = '\n'.join(text_lines)
     
     width = max_width
-    height = sum(font.getsize(line)[1] for line in text_lines) + 10 * len(text_lines)
+    height = sum(font.getbbox(line)[3] for line in text_lines) + 10 * len(text_lines)
 
     img = Image.new("RGBA", (width, height), (255, 255, 255, 0))
     draw = ImageDraw.Draw(img)
     current_height = 0
     for line in text_lines:
         draw.text((0, current_height), line, (0, 0, 0), font)
-        current_height += font.getsize(line)[1] + 10
+        current_height += font.getbbox(line)[3] + 10
 
     with io.BytesIO() as image_binary:
         img.save(image_binary, 'PNG')
