252125903a653e362689acf7cc145ebefef6fd75
og2701 <67282414+og2701@users.noreply.github.com>
2024-06-09 12:10:40 +0100

    update autorestart script

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-until python3 main.py; do
-    echo "The bot crashed with exit code $?. Restarting..." >&2
-    sleep 1
+
+while true
+do
+    echo "Starting bot..."
+    python3 main.py
+    echo "Bot crashed with exit code $?. Restarting in 5 seconds..."
+    sleep 5
 done
