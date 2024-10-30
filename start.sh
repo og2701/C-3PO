3fc2a02f6ce3c69bf8b2ffdafeb72061eed79618
og2701 <67282414+og2701@users.noreply.github.com>
2024-10-30 21:54:25 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+VENV_DIR="/home/ubuntu/C-3PO/venv"
+
+if [ ! -d "$VENV_DIR" ]; then
+    echo "Creating a virtual environment..."
+    python3 -m venv $VENV_DIR
+fi
+
+source $VENV_DIR/bin/activate
+
 python3 -m pip install -r requirements.txt
 
 while true
